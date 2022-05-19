34add62cb707217610d24c6c9d61ff3e001f8c0a
louis-not <mayco.louis@gmail.com>
2022-05-19 21:42:50 +0700

    first trial modeling

--- model_v2.1/userEmbedding/__init__.py
+++ model_v2.1/userEmbedding/__init__.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-"""wishFeatures dataset."""
+"""userEmbedding dataset."""
 
-from .wishFeatures import Wishfeatures
-=======
-"""wishFeatures dataset."""
-
-from .wishFeatures import Wishfeatures
->>>>>>> fe385ef1b97c4ff5f39f6cb3fc228f2fe62857e7
+from .userEmbedding import Userembedding
